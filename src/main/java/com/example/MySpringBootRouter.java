6bc6926df7014f01e52bbcf7d290b9219450a6e7
jorge castro <jorgecastro05@hotmail.com>
2019-02-02 17:39:28 -0500

    Init repo

---
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * A simple Camel route that triggers from a timer and calls a bean and prints to system out.
+ * <p/>
+ * Use <tt>@Component</tt> to make Camel auto detect this route when starting.
+ */
+@Component
+public class MySpringBootRouter extends RouteBuilder {
+
+    @Override
+    public void configure() {
+        restConfiguration().component("restlet").port(9090);
+        rest().get().to("direct:returnBody");
+        from("direct:returnBody")
+                .setHeader("content-type", constant("text/html"))
+                .setHeader("message",simple("{{message}}"))
+                .to("freemarker:templates/response.ftl");
+    }
+
+}
---
+++ src/main/resources/templates/response.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome!</title>
+</head>
+<body>
+  <h1>Welcome To camel Restlet</h1>
+  <h2>The value of property message is: ${headers.message}</h2>
+</body>
+</html>

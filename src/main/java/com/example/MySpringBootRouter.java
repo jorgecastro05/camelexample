b35494db5c6a2d12f4f0c051232ebb65330e5b4f
jorge castro <jorgecastro05@hotmail.com>
2019-02-02 19:09:29 -0500

    Added error handlers

--- src/main/java/com/example/MySpringBootRouter.java
+++ src/main/java/com/example/MySpringBootRouter.java
@@ -1,6 +1,12 @@
 package com.example;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.xmljson.XmlJsonDataFormat;
+import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.spi.DataFormat;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 
@@ -12,14 +18,37 @@ import org.springframework.stereotype.Component;
 @Component
 public class MySpringBootRouter extends RouteBuilder {
 
+    @Bean
+    private DataFormat xmljson() {
+        XmlJsonDataFormat dataFormat = new XmlJsonDataFormat();
+        dataFormat.setRootName("root");
+        dataFormat.setElementName("element");
+        return dataFormat;
+    }
+
     @Override
     public void configure() {
+        getContext().setStreamCaching(true);
+        //default logging error
+        getContext().setErrorHandlerBuilder(deadLetterChannel("direct:defaultErrorHandler"));
         restConfiguration().component("restlet").port(9090);
         rest().get().to("direct:returnBody");
-        from("direct:returnBody")
+        rest().post().to("direct:transformJson");
+
+        from("direct:defaultErrorHandler").routeId("errorHandler")
+                .log(LoggingLevel.ERROR,"${exception.message}")
+                .transform(simple("${exception.message} ${messageHistory}"))
+                .to("freemarker:templates/error.ftl");
+
+        from("direct:returnBody").routeId("routeGet")
                 .setHeader("content-type", constant("text/html"))
-                .setHeader("message",simple("{{message}}"))
+                .setHeader("message", simple("{{message}}"))
                 .to("freemarker:templates/response.ftl");
+
+        from("direct:transformJson").routeId("routePost")
+                .setHeader("content-type", constant("text/xml"))
+                .unmarshal("xmljson");
+
     }
 
 }
---
+++ src/main/resources/templates/error.ftl
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Error</title>
+</head>
+<body>
+  <h1>Welcome To camel Restlet</h1>
+  <h2>There was unhandled error: ${body}</h2>
+</body>
+</html>
